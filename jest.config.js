a973ceb0c26f522b412825c935574d15acb59de3
Guilherme Lacerda <guilherme.lcd@gmail.com>
2019-03-20 21:34:26 -0300

    Criar estrutura inicial com react

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+  modulePaths: ["src"],
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name]-[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: { node: '10' },
+                },
+              ],
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import',
+              '@babel/plugin-proposal-class-properties',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  resolve: {
+    modules: [
+      'src',
+      'node_modules',
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlPlugin({
+      title: 'Transactions Manager',
+      template: 'src/index.html',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      },
+    }),
+  ],
+}
